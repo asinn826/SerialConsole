f872b61aa48ee49911034843c453eb9cf6b43bc9
Alfred Sin <alsin@microsoft.com>
2019-05-16 17:56:05 -0700

    Clarify comment

--- enable_sysrq_nmi.sh
+++ enable_sysrq_nmi.sh
@@ -3,7 +3,7 @@
 # This script will enable your Linux VM for SysRq and NMI by appending to the
 # end of your /etc/sysctl.conf file. Note that if your /etc/sysctl.conf already
 # contains kernel.sysrq=1 or kernel.panic_on_unrecovered_nmi=1 in 
-# /etc/sysctl.conf, this script will probably not change anything
+# /etc/sysctl.conf, this script will probably not change any perceived behavior
 
 echo "kernel.sysrq=1" >> /etc/sysctl.conf
 echo "kernel.panic_on_unrecovered_nmi=1" >> /etc/sysctl.conf
