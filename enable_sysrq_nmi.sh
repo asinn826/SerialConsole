fe8cf16ae8b4fa8ccd166dfb74e91701ded7a846
Alfred Sin <alsin@microsoft.com>
2019-05-16 17:15:10 -0700

    don't use sed

--- enable_sysrq_nmi.sh
+++ enable_sysrq_nmi.sh
@@ -5,6 +5,6 @@
 # contains kernel.sysrq=1 or kernel.panic_on_unrecovered_nmi=1 in 
 # /etc/sysctl.conf, this script will probably not change anything
 
-sed -e "\$akernel.sysrq=1" /etc/sysctl.conf
-sed -e "\$akernel.panic_on_unrecovered_nmi=1" /etc/sysctl.conf
+echo "kernel.sysrq=1" >> /etc/sysctl.conf
+echo "kernel.panic_on_unrecovered_nmi=1" >> /etc/sysctl.conf
 sysctl -p
